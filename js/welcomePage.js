b95e3befa6b18092211b1d85de9fa1387e152045
Renzo <campisirenzo0@gmail.com>
2022-07-22 11:17:52 -0300

    Creacion de carpeta y archivo JS

---
+++ js/welcomePage.js
@@ -0,0 +1,25 @@
+const usernameInput = document.querySelector('#username-input')
+const startButton = document.querySelector('#start-button')
+
+// Implements the button logic
+startButton.addEventListener('click', (event) => {
+  // 1-. Show input if it is 
+  const isInputHidden = usernameInput.classList.contains('hidden')
+
+  if (isInputHidden) {
+    usernameInput.classList.remove('hidden')
+  } 
+  // 2-. Save username and navigate to play
+  else {
+    const username = usernameInput.value
+    const isValidInput = username.trim().length > 0
+
+
+    if (isValidInput) {
+      localStorage.setItem('username', username)
+      window.location.href = '../views/play.html'
+    } else {
+      alert('Invalid username')
+    }
+  }
+})
